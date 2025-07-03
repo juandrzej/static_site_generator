d4beb4bc7db1b0ae50d98dd896a429c9252a5cc4
juandrzej <programming.yippee729@passinbox.com>
2025-07-03 21:50:36 +0200

    refactoring MAJOR MADAFAKA

--- src/markdown_to_html_node.py
+++ src/markdown_to_html_node.py
@@ -4,14 +4,61 @@ from text_to_textnodes import text_to_textnodes
 from textnode import text_node_to_html_node
 
 
-def _code_block_clean(block: str) -> str:
-    """Removes leading and trailing whitespaces and code ticks."""
-    return block[3:-3].strip()
-
-
 def _produce_children(block: str) -> list[LeafNode]:
     """Takes md string which is processed into text nodes and finally html nodes"""
     return [text_node_to_html_node(node) for node in text_to_textnodes(block)]
+
+
+def _code_to_node(block: str) -> ParentNode:
+    """Removes leading and trailing whitespaces and code ticks.
+    Then returns ParentNode which includes all code lines with line breaks."""
+    cleaned_block = block[3:-3].strip()
+    return ParentNode("pre", [LeafNode("code", cleaned_block)])
+
+
+def _paragraph_to_node(block: str) -> ParentNode:
+    # For now removes enters as br tag hasn't been introduced yet
+    block = " ".join(block.split("\n"))
+    children = _produce_children(block)
+    return ParentNode("p", children)
+
+
+def _heading_to_node(block: str) -> ParentNode:
+    heading_count = block[:6].count("#")
+    # Removes hashes first and their trailing space
+    children = _produce_children(block[heading_count + 1 :])
+    return ParentNode(f"h{heading_count}", children)
+
+
+def _quote_to_node(block: str) -> ParentNode:
+    quote_lines = []
+    for line in block.split("\n"):
+        quote_lines.append(line[2:])  # Remove "> "
+    quote_text = " ".join(quote_lines)
+    children = _produce_children(quote_text)
+    return ParentNode("blockquote", children)
+
+
+def _ulist_to_node(block: str) -> ParentNode:
+    parents = []
+    for line in block.split("\n"):
+        # Remove the "- " prefix
+        clean_line = line[2:]
+        # Create individual list item nodes
+        children = _produce_children(clean_line)
+        parents.append(ParentNode("li", children))
+    return ParentNode("ul", parents)
+
+
+def _olist_to_node(block: str) -> ParentNode:
+    parents = []
+    for i, line in enumerate(block.split("\n")):
+        # Remove the "1. ", "2. ", etc. prefix
+        clean_line = line[len(f"{i + 1}. ") :]
+        # Create individual list item nodes
+        children = _produce_children(clean_line)
+        parents.append(ParentNode("li", children))
+    return ParentNode("ol", parents)
 
 
 def block_to_node(block: str) -> ParentNode:
@@ -19,51 +66,33 @@ def block_to_node(block: str) -> ParentNode:
     with corresponding html tag and children included."""
     block_type: BlockType = block_to_block_type_functions(block)
 
-    # Special case; doesn't change anything inside codeblocks
-    if block_type == BlockType.CODE:
-        return ParentNode("pre", [LeafNode("code", _code_block_clean(block))])
-
-    # Return ParentNode based on block_type, mainly to give html tags
-    if block_type == BlockType.PARAGRAPH:
-        # For now removes enters as br tag hasn't been introduced yet
-        block = " ".join(block.split("\n"))
-        children = _produce_children(block)
-        return ParentNode("p", children)
-
-    if block_type == BlockType.QUOTE:
-        # I assume that must be better solution for this but it works for now :P
-        block = " ".join(block.split("\n")).replace("> ", "")
-        children = _produce_children(block)
-        return ParentNode("blockquote", children)
-
-    if block_type == BlockType.HEADING:
-        heading_count: int = block[:6].count("#")
-        # Removes hashes first and their trailing space
-        children = _produce_children(block[heading_count + 1 :])
-        return ParentNode(f"h{heading_count}", children)
-
-    if block_type == BlockType.ULIST:
-        # I assume that must be better solution for this but it works for now :P
-        block = "</li>".join(block.split("\n")).replace("- ", "<li>") + "</li>"
-        children = _produce_children(block)
-        return ParentNode("ul", children)
-
-    if block_type == BlockType.OLIST:
-        # I assume that must be better solution for this but it works for now :P
-        block_lines = block.split("\n")
-        block_lines_cleaned = [
-            item.replace(f"{block_lines.index(item) + 1}. ", "<li>")
-            for item in block_lines
-        ]
-        block = "</li>".join(block_lines_cleaned) + "</li>"
-        children = _produce_children(block)
-        return ParentNode("ol", children)
+    return_dict = {
+        BlockType.CODE: _code_to_node,
+        BlockType.PARAGRAPH: _paragraph_to_node,
+        BlockType.HEADING: _heading_to_node,
+        BlockType.QUOTE: _quote_to_node,
+        BlockType.ULIST: _ulist_to_node,
+        BlockType.OLIST: _olist_to_node,
+    }
+    return return_dict.get(block_type)(block)
+    # # Return ParentNode based on block_type, mainly to give html tags
+    # if block_type == BlockType.CODE:
+    #     return _code_to_node(block)
+    # if block_type == BlockType.PARAGRAPH:
+    #     return _paragraph_to_node(block)
+    # if block_type == BlockType.HEADING:
+    #     return _heading_to_node(block)
+    # if block_type == BlockType.QUOTE:
+    #     return _quote_to_node(block)
+    # if block_type == BlockType.ULIST:
+    #     return _ulist_to_node(block)
+    # if block_type == BlockType.OLIST:
+    #     return _olist_to_node(block)
 
 
 def markdown_to_html_node(markdown: str) -> HTMLNode:
+    """Break markdown string into blocks which are changed to corresponding HTMLNodes.
+    Returns ParentNode which includes all of those nodes."""
     blocks: list[str] = markdown_to_blocks(markdown)
-    parents: list[ParentNode] = []
-    for block in blocks:
-        parents.append(block_to_node(block))
-
+    parents: list[ParentNode] = [block_to_node(block) for block in blocks]
     return ParentNode("div", parents)
