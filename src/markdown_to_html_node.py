759cea821d2131b120df3a9a1cb851559a8e0da9
juandrzej <programming.yippee729@passinbox.com>
2025-07-03 19:59:49 +0200

    p, code and headings work!

--- src/markdown_to_html_node.py
+++ src/markdown_to_html_node.py
@@ -1,9 +1,3 @@
-"""
-Unordered list blocks should be surrounded by a <ul> tag, and each list item should be surrounded by a <li> tag.
-Ordered list blocks should be surrounded by a <ol> tag, and each list item should be surrounded by a <li> tag.
-Headings should be surrounded by a <h1> to <h6> tag, depending on the number of # characters.
-"""
-
 from htmlnode import HTMLNode, LeafNode, ParentNode
 from split_blocks import BlockType, markdown_to_blocks, block_to_block_type_functions
 from text_to_textnodes import text_to_textnodes
@@ -11,22 +5,46 @@ from textnode import text_node_to_html_node
 
 
 def _code_block_clean(block: str) -> str:
-    return "\\n".join(block[3:-3].strip().split("\n"))
+    """Removes leading and trailing whitespaces and code ticks."""
+    return block[3:-3].strip()
+
+
+def _produce_children(block: str) -> list[LeafNode]:
+    """Takes md string which is processed into text nodes and finally html nodes"""
+    return [text_node_to_html_node(node) for node in text_to_textnodes(block)]
 
 
 def block_to_node(block: str) -> ParentNode:
+    """Takes block string and outputs ParentNode
+    with corresponding html tag and children included."""
     block_type: BlockType = block_to_block_type_functions(block)
 
     # Special case; doesn't change anything inside codeblocks
     if block_type == BlockType.CODE:
         return ParentNode("pre", [LeafNode("code", _code_block_clean(block))])
 
-    # First create children as all will use it
-    children = [text_node_to_html_node(node) for node in text_to_textnodes(block)]
+    # For now removes enters as br tag hasn't been introduced yet
+    block = " ".join(block.split("\n"))
+
+    # Return ParentNode based on block_type, mainly to give html tags
     if block_type == BlockType.PARAGRAPH:
+        children = _produce_children(block)
         return ParentNode("p", children)
     if block_type == BlockType.QUOTE:
+        children = _produce_children(block)
         return ParentNode("blockquote", children)
+    if block_type == BlockType.HEADING:
+        heading_count: int = block[:6].count("#")
+        # Removes hashes first and their trailing space
+        children = _produce_children(block[heading_count + 1 :])
+        return ParentNode(f"h{heading_count}", children)
+
+
+"""
+Unordered list blocks should be surrounded by a <ul> tag, and each list item should be surrounded by a <li> tag.
+Ordered list blocks should be surrounded by a <ol> tag, and each list item should be surrounded by a <li> tag.
+Headings should be surrounded by a <h1> to <h6> tag, depending on the number of # characters.
+"""
 
 
 def markdown_to_html_node(markdown: str) -> HTMLNode:
@@ -35,5 +53,4 @@ def markdown_to_html_node(markdown: str) -> HTMLNode:
     for block in blocks:
         parents.append(block_to_node(block))
 
-    print(ParentNode("div", parents).to_html())
     return ParentNode("div", parents)
--- src/test_markdown_to_html_node.py
+++ src/test_markdown_to_html_node.py
@@ -3,23 +3,6 @@ from markdown_to_html_node import markdown_to_html_node
 
 
 class TestMarkdownToHTMLNode(unittest.TestCase):
-    #     def test_paragraphs(self):
-    #         md = """
-    # This is **bolded** paragraph
-    # text in a p
-    # tag here
-    #
-    # This is another paragraph with _italic_ text and `code` here
-    #
-    # """
-    #
-    #         node = markdown_to_html_node(md)
-    #         html = node.to_html()
-    #         self.assertEqual(
-    #             html,
-    #             "<div><p>This is <b>bolded</b> paragraph text in a p tag here</p><p>This is another paragraph with <i>italic</i> text and <code>code</code> here</p></div>",
-    #         )
-
     def test_codeblock(self):
         md = """
 ```
@@ -34,3 +17,72 @@ the **same** even with inline stuff
             html,
             "<div><pre><code>This is text that _should_ remain\nthe **same** even with inline stuff</code></pre></div>",
         )
+
+    def test_paragraphs(self):
+        md = """
+This is **bolded** paragraph
+text in a p
+tag here
+
+This is another paragraph with _italic_ text and `code` here
+
+"""
+
+        node = markdown_to_html_node(md)
+        html = node.to_html()
+        self.assertEqual(
+            html,
+            "<div><p>This is <b>bolded</b> paragraph text in a p tag here</p><p>This is another paragraph with <i>italic</i> text and <code>code</code> here</p></div>",
+        )
+
+    def test_headings(self):
+        md = """
+# This is an h1
+
+## This is an h2
+
+### This is an h3
+
+#### This is an h4
+
+##### This is an h5
+
+###### This is an h6
+"""
+
+        node = markdown_to_html_node(md)
+        html = node.to_html()
+        self.assertEqual(
+            html,
+            "<div><h1>This is an h1</h1><h2>This is an h2</h2><h3>This is an h3</h3><h4>This is an h4</h4><h5>This is an h5</h5><h6>This is an h6</h6></div>",
+        )
+
+    def test_heading_with_inline_markdown(self):
+        md = """
+# This is a **bold** heading
+
+## This has _italic_ text
+
+### Code in `heading` here
+"""
+
+        node = markdown_to_html_node(md)
+        html = node.to_html()
+        self.assertEqual(
+            html,
+            "<div><h1>This is a <b>bold</b> heading</h1><h2>This has <i>italic</i> text</h2><h3>Code in <code>heading</code> here</h3></div>",
+        )
+
+    def test_single_heading(self):
+        md = "## Just one heading"
+
+        node = markdown_to_html_node(md)
+        html = node.to_html()
+        self.assertEqual(
+            html,
+            "<div><h2>Just one heading</h2></div>",
+        )
+
+
+if __name__ == "__main__":
+    unittest.main()
