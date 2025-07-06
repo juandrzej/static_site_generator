8e179720b4924294571a4dae97147706b5725c35
juandrzej <programming.yippee729@passinbox.com>
2025-07-06 14:54:38 +0200

    move tests to separate dir, adjust test.sh, add pyright config so autocompletion in test files still works

--- test.sh
+++ test.sh
@@ -1,2 +1,5 @@
-# This command tells Python to use the standard library's unittest module to run all the tests (discover) it can find in the src directory.
-python3 -m unittest discover -s src
+#!/bin/bash
+# This tells Python where to look for modules
+export PYTHONPATH=src
+# This command tells Python to use the standard library's unittest module to run all the tests (discover) it can find in the tests directory.
+python3 -m unittest discover -s tests/
